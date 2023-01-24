1985e000d5cd2b40637501b10e23ebc4856ef71e
Alessio Limina <alessio.limina@gmail.com>
2023-01-24 11:05:48 +0100

    Exercise: Boolean Operators 3

--- src/it/develhope/bo/Start.java
+++ src/it/develhope/bo/Start.java
@@ -1,2 +1,19 @@
-package it.develhope.io;public class Start {
+package it.develhope.bo;
+
+public class Start {
+    public static void main(String[] args) {
+
+        System.out.println("----------Starting operations----------");
+
+        System.out.println("!(!(!(false ^ false) || (true && true))): " + (!(!(!(false ^ false) || (true && true)))));
+
+        System.out.println("---------------------------------------");
+
+        int x = 3;
+        int y = 2;
+        System.out.println("Considering x=3, y=2: !((x * y) <= 6) && (x - y != 1): " + (!((x * y) <= 6) && (x - y != 1)));
+
+        System.out.println("----------------------------------------");
+
+    }
 }
